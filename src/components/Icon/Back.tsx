c742cf15508c6bd3f6e942436027fad78dcbdee4
rainbowemoji <devzone1209@gmail.com>
2022-05-28 22:32:36 +0900

    feat: Add back button on find and add page

---
+++ src/components/Icon/Back.tsx
@@ -0,0 +1,15 @@
+export default function Back({ stroke = "black" }: { stroke?: string }) {
+  return (
+    <svg
+      width='19'
+      height='19'
+      viewBox='0 0 19 19'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M6.34456 10.0955L18.5 14.0714L18.5 18.2753L0.499999 11.4398L0.5 7.56016L18.5 0.724712L18.5 5.1691L6.34456 9.14504L4.89166 9.62026L6.34456 10.0955Z'
+        stroke={stroke}
+      />
+    </svg>
+  );
+}
