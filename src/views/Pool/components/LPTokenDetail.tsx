811e3861f63f3be8cb09eb9f42e2713f0ecf70d7
typor <dash2hope@gmail.com>
2022-05-29 16:24:21 +0900

    feat: show LP token by details

--- src/views/Pool/components/LPTokenDetail.tsx
+++ src/views/Pool/components/LPTokenDetail.tsx
@@ -2,12 +2,43 @@ import { ReactNode } from "react";
 import casperToken from "../../../assets/images/tokens/0x80dB3a8014872a1E6C3667926ABD7d3cE61eD0C4.svg";
 import swprToken from "../../../assets/images/tokens/0x6FA23529476a1337EB5da8238b778e7122d79666.svg";
 import { Pool } from "../../../store/useWalletStatus";
+import { amountWithoutDecimals } from "../../../utils/utils";
+import { BigNumber } from "ethers";
 
 type LPTokenProps = {
   poolInfo: Pool;
 };
 
 const LPTokenDetail = ({ poolInfo }: LPTokenProps) => {
+  console.log(poolInfo);
+  const balance = amountWithoutDecimals(
+    BigNumber.from(poolInfo.balance),
+    poolInfo.decimals
+  );
+  const shareOfPool =
+    (amountWithoutDecimals(
+      BigNumber.from(poolInfo.balance),
+      poolInfo.decimals
+    ) /
+      amountWithoutDecimals(
+        BigNumber.from(poolInfo.totalSupply),
+        poolInfo.decimals
+      )) *
+    100;
+  const pooledToken0 =
+    (amountWithoutDecimals(
+      BigNumber.from(poolInfo.reserves[0]),
+      poolInfo.tokens[0].decimals
+    ) *
+      shareOfPool) /
+    100;
+  const pooledToken1 =
+    (amountWithoutDecimals(
+      BigNumber.from(poolInfo.reserves[1]),
+      poolInfo.tokens[1].decimals
+    ) *
+      shareOfPool) /
+    100;
   return (
     <div className="font-orator-std w-full py-4 md:py-7 px-2 md:px-14 border border-neutral mt-8 mb-3 md:mt-14 md:mb-8">
       <p className="text-[18px] text-neutral mb-5">lp tokens in your wallet</p>
@@ -30,20 +61,20 @@ const LPTokenDetail = ({ poolInfo }: LPTokenProps) => {
             {poolInfo.tokens[0].symbol}-{poolInfo.tokens[1].symbol} LP
           </p>
         </div>
-        <p>22070</p>
+        <p>{balance}</p>
       </div>
       <div className="text-[15px] text-neutral mt-6">
         <p className="flex justify-between">
           <span>share of pool</span>
-          <span>95.3519%</span>
+          <span>{shareOfPool}%</span>
         </p>
         <p className="flex justify-between my-1">
-          <span>pooled BNB</span>
-          <span>437719</span>
+          <span>pooled {poolInfo.tokens[0].symbol}</span>
+          <span>{pooledToken0}</span>
         </p>
         <p className="flex justify-between">
-          <span>pooled SWPR</span>
-          <span>114.525</span>
+          <span>pooled {poolInfo.tokens[1].symbol}</span>
+          <span>{pooledToken1}</span>
         </p>
       </div>
       <div className="font-orator-std grid gap-4 text-black mt-7">
