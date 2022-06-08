ad046731f4909855e3bfbbd80384ac63c93862eb
rainbowemoji <devzone1209@gmail.com>
2022-06-08 23:10:04 +0900

    fix: Minor update

--- src/components/QuestionHelper/index.tsx
+++ src/components/QuestionHelper/index.tsx
@@ -5,7 +5,7 @@ interface HelperProps {
 
 const QuestionHelper = ({ text, size = "16px" }: HelperProps) => {
   return (
-    <div className="tooltip px-1" data-tip={text}>
+    <div className="flex items-center tooltip px-1" data-tip={text}>
       <button
         className={`w-[16px] h-[16px] border border-neutral text-neutral rounded-[50%] leading-[14px]`}
       >
